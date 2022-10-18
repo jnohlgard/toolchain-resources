e240cd9ba42648ccfa06f6471f627158785ce85e
Joakim Nohlgård <joakim@nohlgard.se>
2022-10-18 17:15:04 +0200

    espressif: Remove any path names from filename

--- espressif/update-urls.sh
+++ espressif/update-urls.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 manifest_url=https://raw.githubusercontent.com/espressif/esp-idf/master/tools/tools.json 
-curl -LfsS "${manifest_url}" | jq -r '.tools[] | select(.name) | .versions[][] | select(. | type=="object") | . += { "file": .url | split("/") | last } | "DIST \(.file) \(.size) URL \(.url) SHA256 \(.sha256)"' | while read tag filename args; do printf '%s %s %s\n' "${tag}" "${filename}" "${args}" > "${filename}.mgv"; done
+curl -LfsS "${manifest_url}" | jq -r '.tools[] | select(.name) | .versions[][] | select(. | type=="object") | . += { "file": .url | split("/") | last } | "DIST \(.file) \(.size) URL \(.url) SHA256 \(.sha256)"' | while read tag filename args; do printf '%s %s %s\n' "${tag}" "${filename}" "${args}" > "${filename##*/}.mgv"; done
