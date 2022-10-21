a0740fa730cff7fc66fa0aa56ff7ed351d9d6a93
Joakim Nohlgård <joakim@nohlgard.se>
2022-10-21 09:56:03 +0200

    clean up update script

--- update-all-and-commit.sh
+++ update-all-and-commit.sh
@@ -3,14 +3,13 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 pwd
-ls -la
 for d in arm espressif; do
-  ls -la "$d"
+  printf 'Updating %s\n' "$d"
   if [ -n "$(git status --porcelain "$d")" ]; then
     >&2 printf 'Skipping unclean %s\n' "$d"
     continue
   fi
-  sh -x "./$d/update-urls.sh"
+  sh "./$d/update-urls.sh"
   if [ -n "$(git status --porcelain "./$d")" ]; then
     git add "./$d"
     git commit "./$d" -m "$d: Sync latest upstream links"
