2237ea910287b576b4230af7a82b35414217fe1d
Joakim Nohlgård <joakim@nohlgard.se>
2022-10-22 08:57:50 +0200

    espressif: fix working dir mistake

--- espressif/update-urls.sh
+++ espressif/update-urls.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-mgv="$(dirname "$0")/../mgv/mgv"
+cd "$(dirname "$0")"
+mgv="${PWD}/../mgv/mgv"
 manifest_url=https://raw.githubusercontent.com/espressif/esp-idf/master/tools/tools.json 
 curl -LfsS "${manifest_url}" | jq -r '.tools[] | select(.name) | .versions[][] | select(. | type=="object") | . += { "file": .url | split("/") | last } | "DIST \(.file) \(.size) URL \(.url) SHA256 \(.sha256)"' | "${mgv}" import
